bb6483a2c9ea0216c26942e2d4e6ec34f409e814
ryan-bower <ryanbower95@gmail.com>
2018-09-07 14:00:13 -0400

    Comments
    Cleaned up my comments

--- app/routes/users.js
+++ app/routes/users.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const db = require('../utilities/db');
 const validator = require('../utilities/validator');
 
-// Returns all users info
+/**
+ * Endpoint for getting all users
+ * Returns all users from the DB
+ */
 router.get('/', function(req, res) {
   db.query('SELECT * FROM employee', [], function(error, results, fields){
     if(error){
@@ -13,12 +16,24 @@ router.get('/', function(req, res) {
   });
 });
 
-// Return info for specified user
+/**
+ * Endpoint for getting a user's information
+ * Returns all of user's information
+ * @param id - the user's id in the DB
+ */
 router.get('/:id', function(req, res) {
   return res.send('Get specified user');
 });
 
-// Add a new user to the database
+/**
+ * Endpoint for adding a new user to the DB
+ * @param fname - user's first name
+ * @param lname - user's last name
+ * @param email - user's email address
+ * @param phone - user's phone number
+ * @param department - user's department
+ * @param password - user's password
+ */
 router.post('/', function(req, res){
   var fname = req.body.fName; 
   var lname = req.body.lName; 
@@ -27,12 +42,11 @@ router.post('/', function(req, res){
   var department = req.body.department; 
   var password = req.body.password;
   
-  //validate the request data
+  // Validate the request data
   var valid = validator.validateRegistrant(fname, lname, email, phone, department, password); 
 
-  valid.then(success => {
-    //validation passed
-    //insert the registrant into the database
+  valid.then(success => { // Validation passed
+    // Insert the registrant into the database
     db.query('INSERT INTO employee(fname, lname, email, phone, departmentID, usertypeID, password, status) ' + 
       'VALUES (?, ?, ?, ?, ?, ?, ?, ?)', 
       [fname, lname, email, phone, department? department.id : null, 3, password, 1], 
@@ -42,10 +56,12 @@ router.post('/', function(req, res){
       }
       return res.send(results);
     });
-  }, err => {
-    //validation failed
+  }, err => { // Validation failed
     return res.status(403).send("Invalid registrant info"); 
   })
 });
 
+/**
+ * Exports the router for use in db.js to listen to members of the connection pool
+ */
 module.exports = router;
