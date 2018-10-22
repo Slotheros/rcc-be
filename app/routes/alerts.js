d75b3751f20b80df4820f705443757e37325df9e
Louis Trapani <lxt2528@rit.edu>
2018-10-22 12:53:16 -0400

    created email/send endpoint

--- app/routes/alerts.js
+++ app/routes/alerts.js
@@ -12,28 +12,28 @@ const authToken = '5320f6abff74555ebdb46f3f09964e45';
 const client = require('twilio')(accountSid, authToken);
 
 router.post('/sms', function(req, res) {
-    var smsMessage = req.body.message;
-    var departments = req.body.departments;
-    //pass in department array array to user->findPhones
-    //returns list of numbers
-    var failedNumbers = []; 
-    var phoneNumbers = User.findPhonesInDepts(departments);
-    phoneNumbers.then(function(numbers){
-        numbers.forEach(function(number){
-            var message = client.messages.create({
-              body: smsMessage,
-              from: '+15853022896',
-              to: number
-            })
-            .then(message => console.log(message.status), error => failedNumbers.push(error? error.to : null))
-            .done();
-        });
-        //returns list of numbers that twilio wasn't able to send the alert to
-        return res.status(200).send(failedNumbers);         
-    }, function(error){
-        console.log(error);
-        return res.status(400).send(error); 
-    });  
+	var smsMessage = req.body.message;
+	var departments = req.body.departments;
+	//pass in department array array to user->findPhones
+	//returns list of numbers
+	var failedNumbers = []; 
+	var phoneNumbers = User.findPhonesInDepts(departments);
+	phoneNumbers.then(function(numbers){
+		numbers.forEach(function(number){
+			var message = client.messages.create({
+				body: smsMessage,
+				from: '+15853022896',
+				to: number
+			})
+			.then(message => console.log(message.status), error => failedNumbers.push(error? error.to : null))
+			.done();
+		});
+		//returns list of numbers that twilio wasn't able to send the alert to
+		return res.status(200).send(failedNumbers);         
+	}, function(error){
+		console.log(error);
+		return res.status(400).send(error); 
+	});  
 });
 
 module.exports = router; 
---
+++ app/routes/email.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+
+'use strict';
+const nodemailer = require('nodemailer');
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+
+// client id - 130825136877-4liulsnpqe55ku7jqldcmvhf8fhjtj64.apps.googleusercontent.com
+// client secret - nORPDE6M-D0SvqZAScZtOm_A
+// refresh token - 1/LPNp0BaoRiGksk7v8r84Ne7b0AK1iDfRYkPMw2U83-Y
+
+
+
+
+router.post('/send', function(req, res) {
+  const message = req.body.message;
+  const email = req.body.email;
+
+
+  const oauth2Client = new OAuth2(
+    "130825136877-4liulsnpqe55ku7jqldcmvhf8fhjtj64.apps.googleusercontent.com", // ClientID
+    "nORPDE6M-D0SvqZAScZtOm_A", // Client Secret
+    "https://developers.google.com/oauthplayground" // Redirect URL
+  );
+    
+  oauth2Client.setCredentials({
+    refresh_token: "1/LPNp0BaoRiGksk7v8r84Ne7b0AK1iDfRYkPMw2U83-Y"
+  });
+        
+  const accessToken = oauth2Client.refreshAccessToken()
+    .then(res => res.credentials.access_token);
+
+  const smtpTransport = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+			type: "OAuth2",
+			user: "teamgarnetgroup@gmail.com", 
+			clientId: "130825136877-4liulsnpqe55ku7jqldcmvhf8fhjtj64.apps.googleusercontent.com",
+			clientSecret: "nORPDE6M-D0SvqZAScZtOm_A",
+			refreshToken: "1/LPNp0BaoRiGksk7v8r84Ne7b0AK1iDfRYkPMw2U83-Y",
+			accessToken: accessToken
+    }
+  });
+
+	const mailOptions = {
+	from: req.body.email,
+	to: "teamgarnetgroup@gmail.com",
+	subject: "Message from " + email + "via the HR Portal",
+	generateTextFromHTML: true,
+	text: message
+	// html: "<p>" + message + "</p>"
+	};
+
+	smtpTransport.sendMail(mailOptions, (error, response) => {
+		error ? console.log(error) : console.log(response);
+		smtpTransport.close(); 
+	});
+
+});
+
+module.exports = router; 
