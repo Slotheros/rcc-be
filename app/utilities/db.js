915a3fdbc64c81666d30a086daebc2bab052c683
Rana Vemireddy <vemireddy.rana@gmail.com>
2018-11-24 22:59:00 -0500

    Changed the DB connection info to point to the db running on the VM

--- app/utilities/db.js
+++ app/utilities/db.js
@@ -1,13 +1,22 @@
 const mysql = require('mysql');
 
+// RIT dev server info
+// const pool = mysql.createPool({
+//   connectionLimit: 10,
+//   host: '129.21.183.59',
+//   user: 'root',
+//   password: 'subversionperversion',
+//   database: 'rrcc'
+// });
+
 /**
  * Creates connection pool
  */
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: '129.21.183.59',
+  host: '10.0.0.239',
   user: 'root',
-  password: 'subversionperversion',
+  password: 'mW(N>CJ9538P',
   database: 'rrcc'
 });
 
